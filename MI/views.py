03aa03f1e14412c178d378b4221acc93890979ee
lucky20chowdary9866 <lakshithakolla@gmail.com>
2024-08-25 22:02:03 +0530

    first commit

---
+++ MI/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def hardik(request):
+    return HttpResponse('<h1><marquee> hardik has good heart</marquee></h1>')
+
+# Create your views here.
